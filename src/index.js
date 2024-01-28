75d7d91e19d9f875f4d9fe18a80f2e613dc82229
Maria c Garcia Everhart <145880784+McEverhart@users.noreply.github.com>
2024-01-27 18:45:48 -0700

    Update index.js
    removed a code in updateTimer

--- src/index.js
+++ src/index.js
@@ -224,7 +224,6 @@ function clearScore() {
 function updateTimer() {
   // TODO: Write your code here.
   // hint: this code is provided to you in the instructions.
-  console.log("updateTimer function called");
   if (time > 0) {
 		time -= 1;
 		timerDisplay.textContent = time;
